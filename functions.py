f08107d65d2ecc40608001f2afe41748ce5199b9
inaki-daat <141517514+inaki-daat@users.noreply.github.com>
2024-05-29 12:07:41 -0600

    Update functions.py

--- functions.py
+++ functions.py
@@ -84,7 +84,7 @@ def description(text):
 def gpt_call(prompt, system):
     client = OpenAI()
     response = client.chat.completions.create(
-                        model="gpt-4-turbo",
+                        model="gpt-4o",
                         messages=[
                             {"role": "system", "content": system},
                             {"role": "user", "content": prompt},
